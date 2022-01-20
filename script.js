e24ef35c354367abd0469f941a3a323a14afd936
ErinCiou <96868999+ErinCiou@users.noreply.github.com>
2022-01-20 16:10:06 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,56 @@
-// slick carousel 滑動輪播效果
+// document ready
 
+$(function() {
+    // 加入導覽列置頂
+    $(function() {
+        var $header = $('.header_wrap');
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > 100) {
+                $header.addClass('fixed');
+                $header.css('padding', '0 80px');
+            } else {
+                $header.removeClass('fixed');
+            }
+        });
 
-$('.center').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    responsive: [{
-            breakpoint: 768,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 3
+    });
+    // smoove滾動載入
+    $('.smv').smoove({
+        min_width: 0,
+        offset: '15%',
+        // opacity: 15,
+    });
+
+    // slick
+
+    $('.slick').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
+
+    $('.center').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [{
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
             }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 1
-            }
-        }
-    ]
+        ]
+    });
+
 });
